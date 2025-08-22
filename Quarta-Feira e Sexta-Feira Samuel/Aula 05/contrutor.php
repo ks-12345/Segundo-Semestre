4791330ca1a49bb6378a553e5c20a23cbb4c3b1b
ks-12345 <brendaalveri1234@gmail.com>
2025-08-22 16:28:09 -0300

    22-08

--- Quarta-Feira e Sexta-Feira Samuel/Aula 05/contrutor.php
+++ Quarta-Feira e Sexta-Feira Samuel/Aula 05/contrutor.php
@@ -7,8 +7,25 @@ public $fornecedor;
 
 public $qtde_estoque;
 
+public function produto_vendido(){
 
-
+ }
 }
+
+// $bolacha = new Produtos("Nikito", "Doces", "Vitarella", 220);
+$bolacha1 = new Produto();
+$bolacha1->nome = "Nikito";
+$bolacha1->categoria = "Doces";
+$bolacha1->fornecedor = "Vitarella";
+$bolacha1->qtde_estoque = 220;
+
+// $feijao = new Produtos("Oliron", "Mantimentos", "Reserva Nobre", 123);
+
+$feijao1 = new Produto();
+$feijao1->nome = "Oliron";
+$feijao1->categoria = "Mantimentos";
+$feijao1->fornecedor = "Reserva Nobre";
+$feijao1->qtde_estoque = 123;
+
 
 ?>
